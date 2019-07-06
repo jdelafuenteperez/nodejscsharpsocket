b760b8f62532872cd372c2ae79b486626dbb1a18
Jose De La Fuente <jdelafuenteperez@gmail.com>
2019-07-06 00:32:49 -0400

    Subida del cliente en C#

---
+++ Client/ClientCSharpNodeSocket/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using Newtonsoft.Json;
+using System.IO;
+namespace ClientCSharpNodeSocket
+{
+    public class ImageConverter : JsonConverter
+    {
+
+        public override bool CanConvert(Type objectType)
+        {
+            //Habilitamos para que pueda convertir imagenes
+            return objectType == typeof(Image);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            //guardamos la imagen en un MemoryStream
+            using (var img = (Image)value)
+            {
+                var ms = new MemoryStream();
+                img.Save(ms, img.RawFormat);
+                ms.Close();
+                byte[] data = ms.ToArray();
+                // Escribimos el la imagen al JsonWriter, la cual será convertida a 
+                // un string base64 por JSON.Net
+                writer.WriteValue(data);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            //Obtenemos el string base64 que contiene la imagen
+            var base64 = (string)reader.Value;
+            // y lo convertimos en la imagen
+            return Image.FromStream(new MemoryStream(Convert.FromBase64String(base64)));
+        }
+    }
+
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        [JsonConverter(typeof(ImageConverter))]
+        public Image Avatar { get; set; }
+
+        public Employee(int ID, string Name, string Surname, DateTime CreationDate, Image Avatar)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.CreationDate = CreationDate;
+            this.Avatar = Avatar;
+        }
+    }
+}
---
+++ Client/ClientCSharpNodeSocket/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ClientCSharpNodeSocket
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Creamos el objeto Recognition
+            Image avatar = Image.FromFile("profile.png");
+
+            Employee _employee = new Employee(1, "Empleado", "Uno", DateTime.Today, avatar);
+
+            //Nos conectamos al servidor
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("127.0.0.1", 8000);
+
+            //Convertimos el objeto Employee a un documento JSON
+            string strEmployee = JsonConvert.SerializeObject(_employee);
+            Console.WriteLine(strEmployee);
+            //Preparamos los datos para enviarlos
+            byte[] data = Encoding.ASCII.GetBytes(strEmployee);
+
+            //Enviamos los datos
+            s.Send(data);
+
+            s.Close();
+            Console.WriteLine("Socket Connected : " + s.Connected);
+            Console.WriteLine("Finish!");
+
+            s.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
